a4fee941585ccf11669dd677299a464e6818c979
Nicholaos Mouzourakis <nevumx@gmail.com>
2016-04-03 21:38:35 -0400

    Small improvement to FPS Counter.

--- Assets/Scripts/SceneObjectScripts/Experimental/FPSCounter.cs
+++ Assets/Scripts/SceneObjectScripts/Experimental/FPSCounter.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using Nx;
 
 public class FPSCounter : MonoBehaviour
 {
+	private	Queue<float>	_deltaTimes	= new Queue<float>();
+
 #if NX_DEBUG
 	private void Awake()
 	{
@@ -9,14 +13,27 @@ public class FPSCounter : MonoBehaviour
 	}
 #endif
 
+	private void Update()
+	{
+		_deltaTimes.Enqueue(Time.deltaTime);
+		if (_deltaTimes.Count > 60)
+		{
+			_deltaTimes.Dequeue();
+		}
+	}
+
 	private void OnGUI()
 	{
+		float sumDeltaTimes = 0.0f;
+		_deltaTimes.ForEach(d => sumDeltaTimes += d);
+		float averageDeltaTime = sumDeltaTimes / _deltaTimes.Count;
+
 		GUI.skin.label.fontSize = 25;
-		if (1.0f / Time.deltaTime < 15.0f)
+		if (1.0f / averageDeltaTime < 15.0f)
 		{
 			GUI.color = Color.red;
 		}
-		else if (1.0f / Time.deltaTime < 30.0f)
+		else if (1.0f / averageDeltaTime < 30.0f)
 		{
 			GUI.color = Color.yellow;
 		}
@@ -24,6 +41,6 @@ public class FPSCounter : MonoBehaviour
 		{
 			GUI.color = Color.green;
 		}
-		GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "FPS: " + Mathf.RoundToInt(1.0f / Time.deltaTime) + " = " + Mathf.RoundToInt(Time.deltaTime * 1000.0f) + "ms");
+		GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "FPS: " + Mathf.RoundToInt(1.0f / averageDeltaTime) + " = " + Mathf.RoundToInt(averageDeltaTime * 1000.0f) + "ms");
 	}
 }
