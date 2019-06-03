79891db1202e0a5129a79225a9c9167370181e79
Nicholaos Mouzourakis <nevumx@gmail.com>
2019-06-02 19:32:30 -0500

    Remove unecessary usings.

--- Assets/Scripts/TweenScripts/TweenHolder.cs
+++ Assets/Scripts/TweenScripts/TweenHolder.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Nx;
 
 public class TweenHolder : MonoBehaviour, ITweenable, IFinishable
