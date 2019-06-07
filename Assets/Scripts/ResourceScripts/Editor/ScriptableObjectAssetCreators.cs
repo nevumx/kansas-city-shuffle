92e9860a6d02ad2037ec7c8955ad83ff9cc88c87
Nicholaos Mouzourakis <nevumx@gmail.com>
2019-06-06 21:34:17 -0500

    Removing unnecessary scripts.

--- Assets/Scripts/ResourceScripts/Editor/ScriptableObjectAssetCreators.cs
+++ Assets/Scripts/ResourceScripts/Editor/ScriptableObjectAssetCreators.cs
@@ -1,24 +1,48 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System;
+using System.IO;
 
 public class ScriptableObjectAssetCreators
 {
 	[MenuItem("Assets/Create/Add New Graphics Setup Asset...")]
 	public static void CreateGraphicsSetupAsset()
 	{
-		CustomAssetUtility.CreateAsset<GraphicsSetup>();
+		CreateAsset<GraphicsSetup>();
 	}
 
 	[MenuItem("Assets/Create/Add New Card Animation Data...")]
 	public static void CreateNewCardAnimationData()
 	{
-		CustomAssetUtility.CreateAsset<CardAnimationData>();
+		CreateAsset<CardAnimationData>();
 	}
 
 	[MenuItem("Assets/Create/Add New Localization Data...")]
 	public static void CreateNewLocalizationData()
 	{
-		CustomAssetUtility.CreateAsset<LocalizationData>();
+		CreateAsset<LocalizationData>();
+	}
+
+	private static void CreateAsset<T>() where T : ScriptableObject
+	{
+		T asset = ScriptableObject.CreateInstance<T>();
+
+		string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+		if (path == "")
+		{
+			path = "Assets";
+		}
+		else if (Path.GetExtension(path) != "")
+		{
+			path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+		}
+
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T) + ".asset");
+
+		AssetDatabase.CreateAsset(asset, assetPathAndName);
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+
+		Selection.activeObject = asset;
 	}
 }
