0581e011aaf1ec176df2b3f9a418c34f359a6e10
Nicholaos Mouzourakis <nevumx@gmail.com>
2019-07-15 22:50:50 -0500

    Small update to FPS Counter.

--- Assets/Scripts/SceneObjectScripts/FPSCounter.cs
+++ Assets/Scripts/SceneObjectScripts/FPSCounter.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 using Nx;
 
 public class FPSCounter : MonoBehaviour
 {
-	private	const		int				DELTA_TIMES_BUFFER_SIZE	= 60;
+	private	const		int				DELTA_TIMES_BUFFER_SIZE	= 180;
 
 	private	readonly	Queue<float>	_deltaTimes				= new Queue<float>();
+	private				float			_cachedMinimumDeltaTime	= 1.0f;
 	private				float			_cachedAverageDeltaTime	= 1.0f;
+	private				float			_cachedMaximumDeltaTime	= 1.0f;
 	private				int				_numDeltaTimesToIgnore	= 3;
+#if UNITY_IOS && !UNITY_EDITOR
+	private				int				_previousTouchCount;
+#endif
 
 	private				bool			_drawFPS;
 
@@ -34,13 +40,16 @@ public class FPSCounter : MonoBehaviour
 #if UNITY_ANDROID && !UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.Menu))
 #elif UNITY_IOS && !UNITY_EDITOR
-		if (Input.touchCount >= 4)
+		if (Input.touchCount != _previousTouchCount && Input.touchCount == 4)
 #else
 		if (Input.GetKeyDown(KeyCode.Escape))
 #endif
 		{
 			_drawFPS = !_drawFPS;
 		}
+#if UNITY_IOS && !UNITY_EDITOR
+		_previousTouchCount = Input.touchCount;
+#endif
 
 		if (_numDeltaTimesToIgnore > 0)
 		{
@@ -54,10 +63,16 @@ public class FPSCounter : MonoBehaviour
 		{
 			_deltaTimes.Dequeue();
 		}
-
+		_cachedMinimumDeltaTime = float.MaxValue;
 		float sumDeltaTimes = 0.0f;
-		_deltaTimes.ForEach(d => sumDeltaTimes += d);
-		_cachedAverageDeltaTime =  sumDeltaTimes / _deltaTimes.Count;
+		_cachedMaximumDeltaTime = float.MinValue;
+		_deltaTimes.ForEach(d =>
+		{
+			_cachedMinimumDeltaTime = Mathf.Min(_cachedMinimumDeltaTime, d);
+			sumDeltaTimes += d;
+			_cachedMaximumDeltaTime = Mathf.Max(_cachedMaximumDeltaTime, d);
+		});
+		_cachedAverageDeltaTime = sumDeltaTimes / _deltaTimes.Count;
 	}
 
 	private void OnGUI()
@@ -80,8 +95,13 @@ public class FPSCounter : MonoBehaviour
 		{
 			GUI.color = Color.green;
 		}
-		GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "FPS: " + Mathf.RoundToInt(1.0f / _cachedAverageDeltaTime)
-						   + " = " + Mathf.RoundToInt(_cachedAverageDeltaTime * 1000.0f) + "ms");
+		GUI.Label(new Rect(0, 0, Screen.width, Screen.height),
+			"FPS: " + Mathf.RoundToInt(1.0f / _cachedAverageDeltaTime)
+				+ " = " + Mathf.RoundToInt(_cachedAverageDeltaTime * 1000.0f) + "ms" + Environment.NewLine
+			+ "Max FPS: " + Mathf.RoundToInt(1.0f / _cachedMinimumDeltaTime)
+				+ " = " + Mathf.RoundToInt(_cachedMinimumDeltaTime * 1000.0f) + "ms" + Environment.NewLine
+			+ "Min FPS: " + Mathf.RoundToInt(1.0f / _cachedMaximumDeltaTime)
+				+ " = " + Mathf.RoundToInt(_cachedMaximumDeltaTime * 1000.0f) + "ms");
 	}
 
 	public bool IsUnderFramerate(float framerate)
@@ -97,6 +117,8 @@ public class FPSCounter : MonoBehaviour
 	{
 		_deltaTimes.Clear();
 		_numDeltaTimesToIgnore = 2;
+		_cachedMinimumDeltaTime = 1.0f;
 		_cachedAverageDeltaTime = 1.0f;
+		_cachedMaximumDeltaTime = 1.0f;
 	}
 }
